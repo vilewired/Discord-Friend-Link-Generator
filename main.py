523f6c694acc5264deb9497e07991140db0872fa
whois-hoeless <whois@hoeless.com>
2023-07-22 12:54:53 +0200

    forgot space here

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ request_header = {
     "Content-Type": "application/json"
 }
 
-payload = { #setting these is useless they will be set automatically
+payload = { # setting these is useless they will be set automatically
 "max_age":0, # max age will be 604800(seconds) (1 week) automatically 
 "max_uses":0, # max uses will be 5 automatically so only 5 people can use it
 "temporary":False
