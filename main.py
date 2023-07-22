2c64fada898f0b7bef300b1de889dee2a8e3930a
whois-hoeless <whois@hoeless.com>
2023-07-22 12:43:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+token = "TOKEN_HERE"
+
+link = 'https://discord.com/api/v9/users/@me/invites'
+request_header = {
+    "Authorization": token,
+    "Content-Type": "application/json"
+}
+
+payload = { #setting these is useless they will be set automatically
+"max_age":0, # max age will be 604800(seconds) (1 week) automatically 
+"max_uses":0, # max uses will be 5 automatically so only 5 people can use it
+"temporary":False
+}
+
+r = requests.post(url=link, headers=request_header, json=payload)
+print(r.text)
+print('discord.gg/' + json.loads(r.text)['code'])
